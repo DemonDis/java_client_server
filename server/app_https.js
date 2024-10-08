f5f0a75e88b98e280b9084d9f7611def66c93fcd
Dima Shibikin <dimart@MacBook-Air-Dima.local>
2024-10-08 17:03:59 +0700

    https del

--- server/app_https.js
+++ server/app_https.js
@@ -1,32 +1,31 @@
-const express = require("express");
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
-
+const express = require('express');
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 const app = express();
-app.use(express.json());
 
-// app.post('/', function (req, res) {
-//   console.log(`Данные: ${JSON.stringify(req.body)}`)
-//   res.json({requestBody: req.body})
-//   // res.send('Сообщение => ' + req.body)
-// })
-app.post('/', (req, res) => {
-  const content = req.body;
-  res.send(`Получено: ${JSON.stringify(content)}`);
-});
+app.use(cors({origin: '*'}));
+app.use(express.json({
+  inflate: true,
+  limit: '100kb',
+  reviver: null,
+  strict: true,
+  type: 'application/json',
+  verify: undefined
+}));
+app.use(express.urlencoded({ extended: true }));
 
+app.route('/article')
+.post((req, res) => {
+    res.send(`Server получил и вернул то же: ${JSON.stringify(req.body)}`);
+    console.log('Client прислал', req.body);
+})
 
-// Read SSL certificate and key files
-const options = {
-  key: fs.readFileSync(path.join(__dirname, "src/certificate/localhost-key.pem")),
-  cert: fs.readFileSync(path.join(__dirname, "src/certificate/localhost.pem")),
-};
+const httpsServer = https.createServer({
+  key: fs.readFileSync('src/certificate/key.pem'),
+  cert: fs.readFileSync('src/certificate/cert.pem'),
+}, app);
 
-// Create HTTPS server
-const server = https.createServer(options, app);
-
-server.listen(4500, () => {
-  console.log(`App listening on https://localhost:4500`);
-
+httpsServer.listen(4500, () => {
+    console.log('HTTPS Server running on port 4500');
 });
