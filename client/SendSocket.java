9e3d6895b599a109cb5b34026e8c7c2ac979a885
DemonDis <dimas.ras@mail.ru>
2024-10-30 18:50:10 +0700

    ⚰️ duplicate imports

--- client/SendSocket.java
+++ client/SendSocket.java
@@ -1,33 +1,5 @@
 import java.util.concurrent.Future;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.InputStream;
-import java.io.Reader;
-import java.io.BufferedReader;
-import java.io.IOException;
-
-import java.net.URL;
 import java.net.URI;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-import javax.json.Json;
-import javax.json.JsonReader;
-import javax.json.JsonObject;
-import javax.json.JsonArray;
-import javax.json.JsonValue;
-import javax.json.JsonString;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.util.log.Log;
@@ -40,8 +12,6 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
-
-import java.security.cert.X509Certificate;
 
 @WebSocket
 public class SendSocket {
